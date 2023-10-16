9ee888bafddee34f45328f8f2873b888e9fbe74f
dxArtur <daniel.arturjsb@gmail.com>
2023-10-16 14:15:42 -0300

    linked navbar pages

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,21 @@
+'use client'
+
 import Link from "next/link"
 import React from "react"
+import { usePathname } from "next/navigation"
 import Image from "next/image"
+import classnames from "classnames"
+import AboutPage from "./pages/about/page"
 import {GiCat} from 'react-icons/gi'
 
 const NavBar = ()=>{
 
+    const currentPath = usePathname()
+
     const links = [
-        {label: 'About', href:'/about'},
-        {label: 'We are', href:'/weare'},
-        {label: 'Contact', href:'/contact'},
+        {label: 'About', href:'/pages/about'},
+        {label: 'We are', href:'/pages/weare'},
+        {label: 'Contact', href:'/pages/contact'},
     ]
 
     return (
@@ -27,7 +34,11 @@ const NavBar = ()=>{
                 {links.map(link =>
                 <Link 
                     key={link.href}
-                    className="text-zinc-500 hover:text-zinc-300 transition-colors"
+                    className={classnames({
+                        'text-zinc-900' : link.href === currentPath,
+                        'text-zinc-500' : link.href !== currentPath,
+                        'hoover: text-zinc-800 transitions-colors' : true
+                    })}
                     href={link.href}>
                         {link.label}   
                 </Link>)}
---
+++ app/pages/weare/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const weArePage = () =>{
+    return (
+        <div className='text-zinc-800'>we are</div>
+    )
+}
+
+export default weArePage
